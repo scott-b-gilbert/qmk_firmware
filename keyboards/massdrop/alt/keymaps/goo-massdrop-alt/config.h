91e1588212a9edbdf04aa8533f3dc9ad53bc6ed5
Scott Gilbert <scott.b.gilbert@protonmail.com>
2021-09-17 21:09:49 -0700

    Fixed tapdance for a-x-c-v, will now select all, cut, copy, paste when held down

--- keyboards/massdrop/alt/keymaps/goo-massdrop-alt/config.h
+++ keyboards/massdrop/alt/keymaps/goo-massdrop-alt/config.h
@@ -1,4 +1,4 @@
 #pragma once
 
 #define ONESHOT_TIMEOUT 3000
-
+#define TAPPING_TERM 175
--- keyboards/massdrop/alt/keymaps/goo-massdrop-alt/keymap.c
+++ keyboards/massdrop/alt/keymaps/goo-massdrop-alt/keymap.c
@@ -37,6 +37,7 @@ enum {
 	TD_X = 0,
     TD_C,
 	TD_V,
+    TD_A,
 	RSHIFT,
 	LSHIFT,
 };
@@ -52,6 +53,8 @@ void c_finished (qk_tap_dance_state_t *state, void *user_data);
 void c_reset (qk_tap_dance_state_t *state, void *user_data);
 void v_finished (qk_tap_dance_state_t *state, void *user_data);
 void v_reset (qk_tap_dance_state_t *state, void *user_data);
+void a_finished (qk_tap_dance_state_t *state, void *user_data);
+void a_reset (qk_tap_dance_state_t *state, void *user_data);
 
 #define CTL_ESC  LCTL_T(KC_ESC)    // Tap for ESC, hold for CTRL
 #define MO_FUNC  MO(_FUNCTION)     // Hold for function layer
@@ -85,8 +88,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_65_ansi_blocker(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME,
-        CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN,
+        CTL_ESC, TD(TD_A),    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,
+        KC_LSFT, KC_Z,    TD(TD_X),TD(TD_C),TD(TD_V),    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN,
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO_FUNC, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
@@ -325,27 +328,54 @@ static tap xtap_state = {
 void x_finished (qk_tap_dance_state_t *state, void *user_data) {
 	xtap_state.state = cur_dance(state);
 	switch (xtap_state.state) {
-		case SINGLE_TAP: register_code16(KC_X); break;
-		case SINGLE_HOLD: register_code16(C(KC_X)); break;
+		case SINGLE_TAP: 
+            register_code16(KC_X); 
+            break;
+		case SINGLE_HOLD: 
+            register_code16(KC_RCTL); 
+            register_code16(KC_X); 
+            break;
 	}
 }
 
 void c_finished (qk_tap_dance_state_t *state, void *user_data) {
 	xtap_state.state = cur_dance(state);
 	switch (xtap_state.state) {
-		case SINGLE_TAP: register_code16(KC_C); break;
-		case SINGLE_HOLD: register_code16(C(KC_C)); break;
+		case SINGLE_TAP: 
+            register_code16(KC_C); 
+            break;
+		case SINGLE_HOLD: 
+            register_code16(KC_RCTL); 
+            register_code16(KC_C);
+            break;
 	}
 }
 
 void v_finished (qk_tap_dance_state_t *state, void *user_data) {
 	xtap_state.state = cur_dance(state);
 	switch (xtap_state.state) {
-		case SINGLE_TAP: register_code16(KC_V); break;
-		case SINGLE_HOLD: register_code16(C(KC_V)); break;
+		case SINGLE_TAP: 
+            register_code16(KC_V); 
+            break;
+		case SINGLE_HOLD: 
+            register_code16(KC_RCTL); 
+            register_code16(KC_V); 
+            break;
 	}
 }
 
+void a_finished (qk_tap_dance_state_t *state, void *user_data) {
+	xtap_state.state = cur_dance(state);
+	switch (xtap_state.state) {
+		case SINGLE_TAP: 
+            register_code16(KC_A); 
+            break;
+		case SINGLE_HOLD: 
+            register_code16(KC_RCTL); 
+            register_code16(KC_A); 
+            break;
+	}
+}
 void lshift_finished (qk_tap_dance_state_t *state, void *user_data) {
 	xtap_state.state = cur_dance(state);
 	switch (xtap_state.state) {
@@ -366,42 +396,77 @@ void rshift_finished (qk_tap_dance_state_t *state, void *user_data) {
 	}
 }
 
-// forgetting keypresses
-
+void a_reset (qk_tap_dance_state_t *state, void *user_data) {
+	switch (xtap_state.state) {
+		case SINGLE_TAP: 
+            unregister_code16(KC_A); 
+            break;
+		case SINGLE_HOLD: 
+            unregister_code16(KC_RCTL); 
+            unregister_code16(KC_A);
+            break;
+	}
+	xtap_state.state = 0;
+}
 void x_reset (qk_tap_dance_state_t *state, void *user_data) {
 	switch (xtap_state.state) {
-		case SINGLE_TAP: register_code16(KC_X); break;
-		case SINGLE_HOLD: register_code16(C(KC_X)); break;
+		case SINGLE_TAP: 
+            unregister_code16(KC_X); 
+            break;
+		case SINGLE_HOLD: 
+            unregister_code16(KC_RCTL); 
+            unregister_code16(KC_X);
+            break;
 	}
 	xtap_state.state = 0;
 }
 
 void c_reset (qk_tap_dance_state_t *state, void *user_data) {
 	switch (xtap_state.state) {
-		case SINGLE_TAP: register_code16(KC_C); break;
-		case SINGLE_HOLD: register_code16(C(KC_C)); break;
+		case SINGLE_TAP: 
+            unregister_code16(KC_C); 
+            break;
+		case SINGLE_HOLD: 
+            unregister_code16(KC_RCTL); 
+            unregister_code16(KC_C);
+            break;
 	}
 	xtap_state.state = 0;
 }
 
 void v_reset (qk_tap_dance_state_t *state, void *user_data) {
 	switch (xtap_state.state) {
-		case SINGLE_TAP: register_code16(KC_V); break;
-		case SINGLE_HOLD: register_code16(C(KC_V)); break;
+		case SINGLE_TAP: 
+            unregister_code16(KC_V); 
+            break;
+		case SINGLE_HOLD: 
+            unregister_code16(KC_RCTL); 
+            unregister_code16(KC_V);
+            break;
 	}
 	xtap_state.state = 0;
 }
+
 
 void lshift_reset (qk_tap_dance_state_t *state, void *user_data) {
 	switch (xtap_state.state) {
-		case SINGLE_TAP: unregister_code16(KC_9); unregister_code16(KC_LSFT); break;
-		case SINGLE_HOLD: unregister_code16(KC_LSFT); break;
-		case DOUBLE_TAP: unregister_code16(KC_LBRC); break;
-		case TRIPLE_TAP: unregister_code16(KC_LBRC); unregister_code16(KC_LSFT); break;
-	}
+		case SINGLE_TAP: 
+            unregister_code16(KC_9); 
+            unregister_code16(KC_LSFT); 
+            break;
+		case SINGLE_HOLD: 
+            unregister_code16(KC_LSFT); 
+            break;
+		case DOUBLE_TAP: 
+            unregister_code16(KC_LBRC); 
+            break;
+		case TRIPLE_TAP: 
+            unregister_code16(KC_LBRC); 
+            unregister_code16(KC_LSFT); 
+            break;
+    } 
 	xtap_state.state = 0;
 }
-
 void rshift_reset (qk_tap_dance_state_t *state, void *user_data) {
 	switch (xtap_state.state) {
 		case SINGLE_TAP: unregister_code16(KC_0); unregister_code16(KC_LSFT); break;
@@ -418,6 +483,6 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 	[RSHIFT] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, rshift_finished, rshift_reset),
 	[TD_C] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, c_finished, c_reset),
 	[TD_V] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, v_finished, v_reset),
-
+	[TD_A] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, a_finished, a_reset)
 };
 
